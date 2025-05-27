2c246797123b8425f40dc4808d9370aecf25e046
Sara <osinska.sara@gmail.com>
2025-05-27 17:24:05 +0200

    puffer's poison is back

--- src/main/java/extendedMechanics/DrawningAnimalsByTheirRarity.java
+++ src/main/java/extendedMechanics/DrawningAnimalsByTheirRarity.java
@@ -13,9 +13,9 @@ public class DrawningAnimalsByTheirRarity implements IAnimalDrawer {
 
     static {
         rarityDrawning.put(Rarity.LEGENDARY, new ArrayList<>());
-        rarityDrawning.put(Rarity.MYTHIC, List.of("Turtle", "Crab", "Shrimp", "Star")); //dla testu usuwam Orca i Whale z MYTHIC i daje do SUPER_RARE
+        rarityDrawning.put(Rarity.MYTHIC, List.of("Whale","Turtle", "Crab", "Shrimp")); //dla testu usuwam Orca i Whale z MYTHIC i daje do SUPER_RARE
         rarityDrawning.put(Rarity.SUPER_RARE, List.of("Shark", "OceanicPuffer","Orca", "Seal"));
-        rarityDrawning.put(Rarity.RARE, List.of("Octopus","Nemo", "Dolphin", "Whale"));
+        rarityDrawning.put(Rarity.RARE, List.of("Octopus","Nemo", "Dolphin",  "Star"));
     }
 
     public static void addTralalero() {
